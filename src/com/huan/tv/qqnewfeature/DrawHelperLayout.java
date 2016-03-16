73d5cabb6d28cacd257154295ebba7184cf37df5
赵红涛 <zhaohngtao@huan.tv>
2016-03-16 18:14:48 +0800

    侧拉抽屉的所有效果实现完毕，新添加了拖拽时主面板和侧拉面板的动画效果，为了兼容低版本，涉及到第三方动画包以及相关计算代码的copy

--- src/com/huan/tv/qqnewfeature/DrawHelperLayout.java
+++ src/com/huan/tv/qqnewfeature/DrawHelperLayout.java
@@ -1,6 +1,10 @@
 package com.huan.tv.qqnewfeature;
 
+import com.nineoldandroids.view.ViewHelper;
+
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.PorterDuff.Mode;
 import android.support.v4.view.ViewCompat;
 import android.support.v4.widget.ViewDragHelper;
 import android.util.AttributeSet;
@@ -12,12 +16,39 @@ import android.widget.FrameLayout;
 public class DrawHelperLayout extends FrameLayout {
 
 	private ViewDragHelper dragHelper;
+	/**左侧被覆盖的抽屉view*/
 	private ViewGroup drawerView;
+	/**最上层的主面板view*/
 	private ViewGroup mainBoardView;
 	private int measuredWidth;
 	private int dragRange;
 	private int measuredHeight;
+	private DragStatusChangedListener dragStatusChangedListener;
+	//初始状态是关闭状态。
+	private DragStatus status = DragStatus.Close;
 
+	/**
+	 * 拖拽状态的枚举
+	 * @author MrTaoge
+	 */
+	public static enum DragStatus{
+		Open,Close,Draging
+	}
+	/**
+	 * 拖拽状态发生改变时的监听。
+	 * @author MrTaoge
+	 *
+	 */
+	public interface DragStatusChangedListener{
+		void open();
+		void close();
+		void draging(float fraction);
+	}
+	
+	public void setDragStatusChangedListener(DragStatusChangedListener dragStatusChangedListener) {
+		this.dragStatusChangedListener = dragStatusChangedListener;
+	}
+	
 	public DrawHelperLayout(Context context) {
 		this(context,null);
 	}
@@ -27,11 +58,11 @@ public class DrawHelperLayout extends FrameLayout {
 	}
 
 	public DrawHelperLayout(Context context, AttributeSet attrs, int defStyle) {
-		super(context,attrs,defStyle);
+		super(context,attrs,defStyle);//这里是调用的父类构造。
 		dragHelper = ViewDragHelper.create(this, 1.0f, new ViewDragHelper.Callback() {
 			//是否可以拖拽当前的控件。
 			@Override
-			public boolean tryCaptureView(View childView, int arg1) {
+			public boolean tryCaptureView(View childView, int pointerId) {
 				return childView==drawerView||childView==mainBoardView;
 			}
 			
@@ -58,17 +89,21 @@ public class DrawHelperLayout extends FrameLayout {
 			public void onViewCaptured(View capturedChild, int activePointerId) {
 				super.onViewCaptured(capturedChild, activePointerId);
 			}
-
+			//当前被捕获到拖拽事件或者位置重置事件的控件的位置发生移动的时候调用，一般决定控件如何移动以及移动的距离等。
 			@Override
 			public void onViewPositionChanged(View changedView, int left,int top, int dx, int dy) {
 				super.onViewPositionChanged(changedView, left, top, dx, dy);
-				int mainBoardLeftPadding = mainBoardView.getLeft();
-				if(changedView==drawerView){
-					mainBoardLeftPadding = mainBoardLeftPadding+dx;
+				int mainBoardLeftMargin = mainBoardView.getLeft();
+				if(changedView==drawerView){//通过左侧抽屉控件的位移来控制主面板的移动，与此同时，屏蔽抽屉控件的移动。
+					mainBoardLeftMargin = mainBoardLeftMargin+dx;
 					drawerView.layout(0, 0, measuredWidth, measuredHeight);//左侧的抽屉面板不允许移动，也就是每次有移动事件时，都把它的位置重置，就形成了没有动的效果。
-					mainBoardLeftPadding = restrictEdgeWhenDrag(mainBoardLeftPadding);
-					mainBoardView.layout(mainBoardLeftPadding, 0, mainBoardLeftPadding+measuredWidth, 0+measuredHeight);
+					mainBoardLeftMargin = restrictEdgeWhenDrag(mainBoardLeftMargin);
+					mainBoardView.layout(mainBoardLeftMargin, 0, mainBoardLeftMargin+measuredWidth, 0+measuredHeight);
 				}
+				//分发拖拽事件
+				
+				dispatchDragEvent(mainBoardLeftMargin);
+				
 				invalidate();//兼容2.3版本的拖拽事件，否则在2.3版本上不能实现拖拽。
 			}
 			
@@ -84,15 +119,107 @@ public class DrawHelperLayout extends FrameLayout {
 					closeDrawer(true);
 				}
 			}
-			
 		});
 	}
 	
+	/**
+	 * 分发拖拽事件
+	 * @param mainBoardLeftMargin 主面板左端距离页面的距离。
+	 */
+	protected void dispatchDragEvent(int mainBoardLeftMargin) {
+		float motionPercent = mainBoardLeftMargin*1.0f/dragRange;//算出主面板在可拖拽范围内的水平方向上的移动百分比。
+		playAnimation(motionPercent);
+		//刷新拖拽过程中的状态。
+		updateDragStatus(motionPercent);
+	}
+	/**
+	 * 更新拖拽状态。
+	 * @param motionPercent
+	 */
+	private void updateDragStatus(float motionPercent) {
+		DragStatus lastStatus = status;//不断的获取上一次刷新时的状态。
+		if(motionPercent==0){
+			status = DragStatus.Close;
+		}else if(motionPercent==1){
+			status = DragStatus.Open;
+		}else{
+			status = DragStatus.Draging;
+		}
+		if(dragStatusChangedListener!=null){
+			dragStatusChangedListener.draging(motionPercent);
+		}
+		if(status!=lastStatus&&dragStatusChangedListener!=null){
+			if(status==DragStatus.Open){
+				dragStatusChangedListener.open();
+			}else if(status==DragStatus.Close){
+				dragStatusChangedListener.close();
+			}else{
+				dragStatusChangedListener.draging(motionPercent);
+			}
+		}
+	}
+	/**
+	 * 播放拖拽时的动画。
+	 * @param motionPercent
+	 */
+	private void playAnimation(float motionPercent) {
+		//主面板的缩放动画
+		ViewHelper.setScaleX(mainBoardView, evaluate(motionPercent, 1.0f, 0.8f));
+		ViewHelper.setScaleY(mainBoardView, evaluate(motionPercent, 1.0f, 0.8f));
+		//左侧抽屉的缩放动画
+		ViewHelper.setScaleX(drawerView, evaluate(motionPercent, 0.5f, 1.0f));
+		ViewHelper.setScaleY(drawerView, evaluate(motionPercent, 0.5f,1.0f ));
+		//抽屉的透明度变化
+		ViewHelper.setAlpha(drawerView, evaluate(motionPercent, 0.0f, 1.0f));
+		//抽屉的位移动画
+		ViewHelper.setTranslationX(drawerView, evaluate(motionPercent, - measuredWidth*0.5f, 0.0f));
+		//设置背景的渐变
+		getBackground().setColorFilter(evaluateColor(motionPercent,Color.BLACK,Color.TRANSPARENT),Mode.SRC_OVER);
+	}
+	/**
+	 * 计算颜色值的渐变。
+	 * @param motionPercent
+	 * @param black
+	 * @param transparent
+	 * @return
+	 */
+	private int evaluateColor(float motionPercent, int startValue, int endValue) {
+		int startInt = (Integer) startValue;
+        int startA = (startInt >> 24) & 0xff;
+        int startR = (startInt >> 16) & 0xff;
+        int startG = (startInt >> 8) & 0xff;
+        int startB = startInt & 0xff;
+
+        int endInt = (Integer) endValue;
+        int endA = (endInt >> 24) & 0xff;
+        int endR = (endInt >> 16) & 0xff;
+        int endG = (endInt >> 8) & 0xff;
+        int endB = endInt & 0xff;
+
+        return (int)((startA + (int)(motionPercent * (endA - startA))) << 24) |
+                (int)((startR + (int)(motionPercent * (endR - startR))) << 16) |
+                (int)((startG + (int)(motionPercent * (endG - startG))) << 8) |
+                (int)((startB + (int)(motionPercent * (endB - startB))));
+	}
+	
+	/**
+	 * 计算开始值与结束值之间的渐变。
+	 * @param percent
+	 * @param startValue
+	 * @param endValue
+	 * @return
+	 */
+	public float evaluate(float percent,Number startValue,Number endValue){
+		float startFloat = startValue.floatValue();
+        return startFloat + percent * (endValue.floatValue() - startFloat);
+	}
+	//类似于ViewDragHelper和Scroller这类有可以定义控件滚动事件的类来说，这个方法是应该重写的，否则控件虽然可以移动，但是像松手后可以自行平滑的移动一段距离的这种事件却不能执行。也就是说"打一鞭子走一下"的生硬效果，而不带有缓冲效果。
 	@Override
 	public void computeScroll() {
 		super.computeScroll();
 		if(dragHelper.continueSettling(true)){
-			ViewCompat.postInvalidateOnAnimation(this);
+			ViewCompat.postInvalidateOnAnimation(this);//相当于invalidate方法。
+//			invalidate();
 		}
 	}
 	/**
@@ -124,7 +251,7 @@ public class DrawHelperLayout extends FrameLayout {
 	}
 
 	/**
-	 * 当对控件进行拖拽时，限制控件的边缘位置。
+	 * 当对控件进行拖拽时，限制控件的边缘位置，也就是屏蔽类似于0和超越屏幕宽度的这些非法值。
 	 * @param left
 	 * @return
 	 */
--- src/com/huan/tv/qqnewfeature/MainActivity.java
+++ src/com/huan/tv/qqnewfeature/MainActivity.java
@@ -1,15 +1,70 @@
 package com.huan.tv.qqnewfeature;
 
+import java.util.Random;
+
+import android.animation.ObjectAnimator;
+import android.annotation.TargetApi;
 import android.app.Activity;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
+import android.view.animation.CycleInterpolator;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.huan.tv.qqnewfeature.DrawHelperLayout.DragStatusChangedListener;
+import com.nineoldandroids.view.ViewHelper;
 
 public class MainActivity extends Activity {
 
+	private ListView listView_left,listView_main;
+	private ImageView iv_head;
+
+	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_main);
+		iv_head = (ImageView) findViewById(R.id.iv_head_main);
+		listView_left = (ListView) findViewById(R.id.listView_left);
+		listView_main = (ListView) findViewById(R.id.list_main);
+		listView_left.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, NamesSet.EnglishNames){
+			@Override
+			public View getView(int position, View convertView, ViewGroup parent) {
+				TextView tv_name = (TextView) super.getView(position, convertView, parent);
+				tv_name.setTextColor(Color.WHITE);
+				return tv_name;
+			}
+		});
+		listView_main.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, NamesSet.ChineseNames));
+		DrawHelperLayout rootView = (DrawHelperLayout) findViewById(R.id.root_view);
+		rootView.setDragStatusChangedListener(new DragStatusChangedListener() {
+			
+			public void open() {
+				Toast.makeText(MainActivity.this, "打开", 0).show();
+				listView_left.smoothScrollToPosition(new Random().nextInt(30));
+			}
+			
+			public void draging(float fraction) {
+//				Toast.makeText(MainActivity.this, "拖拽", 0).show();
+				ViewHelper.setAlpha(iv_head, 1-fraction);
+			}
+			
+			public void close() {
+				Toast.makeText(MainActivity.this, "关闭", 0).show();
+				ObjectAnimator animator = ObjectAnimator.ofFloat(iv_head, "translationX", 15f);
+				animator.setInterpolator(new CycleInterpolator(2.0f));
+				animator.setDuration(500);
+				animator.start();
+				
+			}
+		});
 	}
 }
